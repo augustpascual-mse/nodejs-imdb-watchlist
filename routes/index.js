67bba230186b97da4f915a17a7e1b2e6811bb999
August Pascual <augustpascual.mse@gmail.com>
2017-05-06 15:44:37 -0700

    Update spacing

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,33 @@ router.get('/movies', function(req, res, next) {
 
   var imageUrl = [];
   var imageCount = 0;
-    movies.getMovies.forEach(function(movieId){
-    var promise = new Promise(function(resolve, reject) {
-      http.get("http://omdbapi.com?i=" + movieId + "&plot=full&r=json", (res) => {
-        imageCount++;
-        let data = '';
 
-        res.on("data", function(chunk) {
-          data = data + chunk;
-        });
+  movies.getMovies.forEach(function(movieId){
+      var promise = new Promise(function(resolve, reject) {
+          http.get("http://omdbapi.com?i=" + movieId + "&plot=full&r=json", (res) => {
+              imageCount++;
+              let data = '';
 
-        res.on("end", function(){
-          let jsonData = JSON.parse(data);
-          imageUrl.push(jsonData.Poster);
-          if (imageCount == movies.getMovies.length) {
-              resolve(imageUrl);
-          }
-        });
+              res.on("data", function(chunk) {
+                  data = data + chunk;
+              });
 
+              res.on("end", function(){
+                  let jsonData = JSON.parse(data);
+                  imageUrl.push(jsonData.Poster);
+                  if (imageCount == movies.getMovies.length) {
+                      resolve(imageUrl);
+                  }
+              });
+          });
+      })
+
+      promise.then((imageUrl) => {
+          res.render('movies', { imageUrl: imageUrl });
       });
-    })
-
-    promise.then((imageUrl) => {
-      res.render('movies', { imageUrl: imageUrl });
-    });
 
   });
 
-
-
 });
-
 
 module.exports = router;
